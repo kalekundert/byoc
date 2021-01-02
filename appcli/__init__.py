9694681a33852005a48a6c609ba12af6bd56b213
Kale Kundert <kale@thekunderts.net>
2021-01-02 13:04:31 -0500

    feat: export the lookup() function

--- appcli/__init__.py
+++ appcli/__init__.py
@@ -9,6 +9,7 @@ __version__ = '0.1.0'
 from .model import init, load
 from .params import param
 from .attrs import config_attr
+from .utils import lookup
 from .layers import *
 from .config import *
 from .errors import *
