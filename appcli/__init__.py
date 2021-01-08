ea7ba8980848a0f6c46f2d0f293769ff76490ad3
Kale Kundert <kale@thekunderts.net>
2021-01-08 15:25:29 -0500

    feat: add an easy way to toggle boolean parameters

--- appcli/__init__.py
+++ appcli/__init__.py
@@ -9,9 +9,9 @@ __version__ = '0.4.0'
 from .app import App, FromParams
 from .model import init, load, reload
 from .params import param, Key
+from .toggle import Toggle as toggle, pick_toggled, toggle_param
 from .attrs import config_attr
 from .utils import lookup
 from .layers import *
 from .config import *
 from .errors import *
-
